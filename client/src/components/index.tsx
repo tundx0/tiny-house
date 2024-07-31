f08a64277b27c188e5c15ab86d666686372add65
Olatunde Adegboyebo <tundx0@gmail.com>
2024-07-31 13:51:42 +0100

    client: add new components

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,2 +1,8 @@
-import Listings from "./Listings/Listings";
-export { Listings };
+import Listings from "./Listings";
+import Listing from "./Listing";
+import Home from "./Home";
+import Host from "./Host";
+import NotFound from "./NotFound";
+import User from "./User";
+
+export { Listings, Listing, Home, Host, NotFound, User };
