b8b7a478a1d24cddb3e496ba837ffc80a46ccd86
Olatunde Adegboyebo <tundx0@gmail.com>
2024-08-15 23:58:25 +0100

    add custom domain to enable cors and cookie authentication

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // Allows Vite to be accessed by custom domains
+    port: 5173, // The port for mydomain.com
+    proxy: {
+      "/api": {
+        target: "http://api.domain.com:9000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
