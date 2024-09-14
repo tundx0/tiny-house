83f4312fb4f1c39386d6561ed7bd439844449810
Olatunde Adegboyebo <tundx0@gmail.com>
2024-09-14 01:37:58 +0100

    client: path alias setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,15 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     host: true, // Allows Vite to be accessed by custom domains
     port: 5173, // The port for mydomain.com
